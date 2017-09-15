0feb88f68932826189a9c4cb9aea16bb5d5364b7
sam quave <samquave@sams-air.chattanoogatn.gov>
2017-09-15 09:00:49 -0400

    .draw

--- script.js
+++ script.js
@@ -1,27 +1,70 @@
-var body = document.getElementsByTagName('body')[0];
+var inputSquareSide = document.getElementById('square-side-box');
+var inputRectHeight = document.getElementById('rect-height-box');
+var inputRectWidth = document.getElementById('rect-width-box');
+var inputTriHeight = document.getElementById('tir-height-box');
+var inputCircleRadius = document.getElementById('cir-radius-box');
 
-var Shape = function () {
-    this.p.createElement('p');
-    var canvas = document.getElementsByClassName('canvas');
-    canvas.appendChild(this.p);
+var shapeName = document.getElementById('shape-name-detail');
+var widthDetail = document.getElementById('width-detail');
+var heightDetail = document.getElementById('height-detail');
+var radiusDetail = document.getElementById('radius-detail');
+var areaDetail = document.getElementById('area-detail');
+var perimeterDetail = document.getElementById('perimeter-detail');
+
+document.getElementById('square-submit').addEventListener('click', drawCircle());
+ 
+function drawCircle() {
+    var inputRadius = inputCircleRadius.value;
+
+new Circle(inputRadius);
+}
+
+function Shape(width, height) {
+    this.width = width;
+    this.height = height;
 
 };
 
-var Square = function(height) {
-    
-    this.height = document.getElementById('square-side-box').value;
-    this.p.style.backgroundColor = red;
-    var squarebtn = document.getElementById('square-submit');
-    squarebtn.addEventListener('click', this.drawSquare.bind(this));
-
+Shape.prototype.draw = function() {
+    this.div = document.createElement('div');
+    this.div.className = 'shape ' + this.cssClass;
+    var x = Math.floor(Math.random()*600-this.width);
+    var y = Math.floor(Math.random()*600-this.height);
+    this.div.style.top = y +'px';
+    this.div.style.left = x + 'px';
+    this.div.style.width = this.width;
+    this.div.style.height = this.height;
+    this.div.addEventListener('click', this.describe.bind(this));
+    this.div.addEventListener('dblclick', function() {
+        this.div.remove();
+    }.bind(this));
+    canvas.appendChild(this.div);
 };
-Square.prototype=Object.create(Shape.prototype);
-Square.prototype.constructor= Square;
 
-function drawSquare(){
-    var square = document.createElement('div');
-    square.className = 'square';
-    square.style.backgroundColor = red;
-    square.style.height = this.height;
-    square.style.width = this.height;
+Shape.prototype.describe = function() {
+    shapeName = this.constructor.name;
+    widthDetail = this.width;
+    heightDetail = this.height;
+    radiusDetail = this.radius;
+    areaDetail = this.area();
+    perimeterDetail = this.perimeter();
+};
+
+function Circle(radius) {
+   Shape.call(this, 2*radius, 2*radius);
+   this.radius = radius;
+   this.cssClass = 'circle';
+   this.draw();
+   
+};
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.area = function() {
+    return Math.PI*Math.pow(this.radius, 2);
+}
+
+Circle.prototype.area = function() {
+    return 2*Math.PI*this.radius;
 }
